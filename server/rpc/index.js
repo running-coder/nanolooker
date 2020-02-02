a42a9dde012902e636c546829b478056f861cfd9
tombertrand <tom.bertrand@lightspeedhq.com>
2020-02-01 23:02:48 -0500

    modify deploy script to remove unused files

--- server/rpc/index.js
+++ server/rpc/index.js
@@ -9,8 +9,6 @@ const {
 
 const rpcCache = new NodeCache();
 
-console.log("rpcCache", rpcCache);
-
 const transformers = {
   confirmation_quorum,
   representatives
@@ -62,7 +60,6 @@ const rpc = async (action, params) => {
     json = cacheKey && rpcCache.get(cacheKey);
 
     if (!json) {
-      console.log(`No cache found for: ${cacheKey}`);
       const body = JSON.stringify({
         jsonrpc: "2.0",
         action,
