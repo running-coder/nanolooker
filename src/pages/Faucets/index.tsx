417dae6a7f50c479b63930e7d0e210f293cc8ea0
tombertrand <tom.bertrand@lightspeedhq.com>
2021-02-27 17:02:47 -0500

    Fix Faucets display on mobile

--- src/pages/Faucets/index.tsx
+++ src/pages/Faucets/index.tsx
@@ -6,25 +6,25 @@ const { Title, Text } = Typography;
 
 const faucets = [
   {
-    name: "nano-faucet.org",
+    name: "nano-faucet",
     account:
       "nano_34prihdxwz3u4ps8qjnn14p7ujyewkoxkwyxm3u665it8rg5rdqw84qrypzk",
     link: "https://nano-faucet.org/",
   },
   {
-    name: "FreeNanoFaucet.com",
+    name: "FreeNanoFaucet",
     account:
       "nano_3kwppxjcggzs65fjh771ch6dbuic3xthsn5wsg6i5537jacw7m493ra8574x",
     link: "https://www.freenanofaucet.com/",
   },
   {
-    name: "Apollo Twitter bot Faucet",
+    name: "Apollo Faucet",
     account:
       "nano_1tyd79peyzk4bs5ok1enb633dqsrxou91k7y4zzo1oegw4s75bokmj1pey4s",
     link: "https://twitter.com/ApolloNano/status/1365520666137481220",
   },
   {
-    name: "LuckyNano.com - Faucet 💰 - Casino 🎰 - Main Rep 💪🏼",
+    name: "LuckyNano",
     account:
       "nano_1oenixj4qtpfcembga9kqwggkb87wooicfy5df8nhdywrjrrqxk7or4gz15b",
     link: "https://luckynano.com/",
@@ -46,15 +46,19 @@ const FaucetsPage = () => {
         <Descriptions bordered column={1} size="small">
           {faucets.map(({ name, account, link }) => (
             <Descriptions.Item label={name}>
-              <div>
-                <Link
-                  to={`/account/${account}`}
-                  className="break-word color-normal"
-                >
-                  {account}
-                </Link>
-              </div>
-              <a href={link} target="_blank" rel="noopener noreferrer">
+              <Link
+                to={`/account/${account}`}
+                className="break-word color-normal"
+              >
+                {account}
+              </Link>
+              <br />
+              <a
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="break-word"
+              >
                 {link}
               </a>
             </Descriptions.Item>
