df34d41cb44404e429d612af4c560107dd6b1bfc
tombertrand <tom.bertrand@lightspeedhq.com>
2021-07-02 11:22:49 -0400

    Identify NanoBrowserQuest

--- src/pages/Home/Banner.tsx
+++ src/pages/Home/Banner.tsx
@@ -6,7 +6,7 @@ import { Theme, PreferencesContext } from "api/contexts/Preferences";
 import Search from "components/Search";
 import { Tracker } from "components/utils/analytics";
 
-const { Text, Title } = Typography;
+const { Title } = Typography;
 
 const Banner: React.FC = () => {
   const { t } = useTranslation();
@@ -64,49 +64,6 @@ const Banner: React.FC = () => {
         <Link to={"/what-is-nano"}>
           <Button ghost>{t("menu.whatIsNano")}</Button>
         </Link>
-
-        <Button
-          ghost
-          href="https://pasino.com/?user_id=18828"
-          target="_blank"
-          style={{ padding: "0 10px" }}
-          onClick={() => {
-            Tracker.ga4?.gtag("event", "SponsorPasino");
-          }}
-        >
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <span
-              className="anticon"
-              style={{
-                color: "gold",
-                display: "flex",
-                alignItems: "center",
-                height: "100%",
-              }}
-            >
-              <svg
-                viewBox="0 0 1280 1024"
-                focusable="false"
-                data-icon="dice"
-                width="24.75px"
-                height="20px"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  d="M1184 384H946.52c25.38 59.18 14.24 130.4-34 178.64L640 835.16V928c0 53.02 42.98 96 96 96h448c53.02 0 96-42.98 96-96V480c0-53.02-42.98-96-96-96zM960 752c-26.5 0-48-21.5-48-48 0-26.52 21.5-48 48-48s48 21.48 48 48c0 26.5-21.5 48-48 48z m-92.74-373.4L517.4 28.74c-38.32-38.32-100.46-38.32-138.78 0L28.74 378.6c-38.32 38.32-38.32 100.46 0 138.78L378.6 867.26c38.32 38.32 100.46 38.32 138.78 0L867.26 517.4c38.32-38.34 38.32-100.48 0-138.8zM192 496c-26.5 0-48-21.5-48-48 0-26.52 21.5-48 48-48s48 21.48 48 48c0 26.5-21.5 48-48 48z m256 256c-26.5 0-48-21.5-48-48 0-26.52 21.5-48 48-48s48 21.48 48 48c0 26.5-21.5 48-48 48z m0-256c-26.5 0-48-21.5-48-48 0-26.52 21.5-48 48-48s48 21.48 48 48c0 26.5-21.5 48-48 48z m0-256c-26.5 0-48-21.5-48-48 0-26.52 21.5-48 48-48s48 21.48 48 48c0 26.5-21.5 48-48 48z m256 256c-26.5 0-48-21.5-48-48 0-26.52 21.5-48 48-48s48 21.48 48 48c0 26.5-21.5 48-48 48z"
-                  p-id="1483"
-                ></path>
-              </svg>
-            </span>
-            <Text
-              //@ts-ignore
-              style={{ marginLeft: "6px", color: "gold", fontWeight: "500" }}
-            >
-              Pasino
-            </Text>
-          </div>
-        </Button>
 
         <Link to={"/nanoquakejs"}>
           <Button ghost>
