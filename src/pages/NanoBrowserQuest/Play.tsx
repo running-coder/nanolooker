b073a063092d3d797412354e3f74ea63cb5db28b
tombertrand <tom.bertrand@lightspeedhq.com>
2022-05-01 20:09:23 -0400

    Remove nbq promotion

--- src/pages/NanoBrowserQuest/Play.tsx
+++ src/pages/NanoBrowserQuest/Play.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
 import { useTranslation } from "react-i18next";
-import { Button, Typography } from "antd";
+import { Button } from "antd";
 import { Tracker } from "components/utils/analytics";
-
-const { Text } = Typography;
 
 const Play: React.FC = () => {
   const { t } = useTranslation();
@@ -11,28 +9,6 @@ const Play: React.FC = () => {
 
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          width: "70%",
-          border: "solid 1px #f0f2f5",
-          margin: "12px auto",
-          padding: 12,
-        }}
-      >
-        <img
-          alt="NanoBrowserQuest Promotion 50% off Freezing Lands expansion"
-          src={`/nanobrowserquest/50-off.png`}
-          height="62px"
-          width="100px"
-        />
-        <Text strong style={{ textAlign: "left", marginLeft: 12 }}>
-          Speak to the Wizard in town to purchase the Freezing Lands expansion.
-          Other store items are also on sale!
-        </Text>
-      </div>
       <Button
         type="primary"
         size="large"
