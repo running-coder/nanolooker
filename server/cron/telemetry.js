c172f371406005b219915b993f53e8aa04a30275
Olivier <olaverre@free.fr>
2022-11-08 13:14:41 +0100

    Enable Telemetry

--- server/cron/telemetry.js
+++ server/cron/telemetry.js
@@ -190,10 +190,9 @@ const calculateAverage = (values, percentile) => {
 cron.schedule("*/10 * * * *", async () => {
   if (process.env.NODE_ENV !== "production") return;
 
-  // @NOTE Telemetry is not available on Banano
-  // doTelemetryCron();
+     doTelemetryCron();
 });
 
-// if (process.env.NODE_ENV === "production") {
-//   doTelemetryCron();
-// }
+if (process.env.NODE_ENV === "production") {
+     doTelemetryCron();
+}
